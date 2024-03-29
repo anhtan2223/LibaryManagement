f57d11897f42e624a179664d297338dc1f28fbf5
anhtan2223 <anhtan3332@gmail.com>
2024-03-30 00:30:00 +0700

    Add Axios , Connect MySQL success

--- Code/Backend/app/config/config.js
+++ Code/Backend/app/config/config.js
@@ -1,5 +1,11 @@
 module.exports = {
     app : {
         port : 3000
+    } ,
+    database : {
+        host : 'localhost' ,
+        user : 'root' ,
+        password : 'anhtan2oo3' ,
+        database : 'FishLearning' ,
     }
 }
--- Code/Backend/server.js
+++ Code/Backend/server.js
@@ -1,10 +1,22 @@
 const app = require('./app')
 const config = require('./app/config/config')
-
+const MySQL = require('./app/service/mysql.service')
 const port = config.app.port
-app.listen(
-    port ,
-    () => {
-        console.log(`Server Now Running at Port ${3000}`)
-    }
-)
+
+
+async function runServer()
+{
+    await MySQL.connect(config.database)
+    MySQL.con.connect((err)=>{
+        if(err){
+            console.log("Connect Fail" , err);
+            return 
+        }
+        console.log(`Connect To ${config.database.database} Success`);
+    })
+
+    await app.listen( port , () => {
+        console.log(`Server Now Running at Port ${3000}`) })
+}
+
+runServer()
