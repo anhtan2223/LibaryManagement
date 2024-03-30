ccc6d2c6226b779ded2d68913e9d0ddb4b6b4421
PHP <phphong211@gmail.com>
2024-03-30 11:03:36 +0700

    another sidebar

--- Code/Frontend/src/components/Sidebar.vue
+++ Code/Frontend/src/components/Sidebar.vue
@@ -2,14 +2,45 @@
     <body class="wrapper">
         <aside id="sidebar">
             <div class="d-flex">
-                <button id="toggle-btn" type="button">
-                    <i class="lni lni-book"></i>
-                </button>
-                <div class="sidebar-logo">
-                    <a href="#">Library Management</a>
+                <div id="toggle-btn" class="sidebar-logo">
+                    <i class="fa-solid fa-book-open"></i>
+                </div>
+                <div class="sidebar-title">
+                    <a href="#"> Library Management</a>
                 </div>
             </div>
+            <ul class="sidebar-nav">
+                <li class="sidebar-item">
+                    <a href="" class="sidebar-link">
+                        <i class="lni lni-home"></i>
+                        <span> Trang Chủ</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="" class="sidebar-link">
+                        <i class="lni lni-user"></i>
+                        <span> Quản lý người đọc</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="" class="sidebar-link">
+                        <i class="lni lni-book"></i>
+                        <span> Quản lý sách</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="" class="sidebar-link">
+                        <i class="lni lni-write"></i>
+                        <span> Quản lý tác giả</span>
+                    </a>
+                </li>
+            </ul>
         </aside>
+        <div class="main-p3">
+            <div class="text-center">
+                <h1>something will be here</h1>
+            </div>
+        </div>
     </body>
 </template>
 
