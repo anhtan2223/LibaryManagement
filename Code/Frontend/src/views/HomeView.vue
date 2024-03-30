77a1115de44f7ba92508c77c3d6a9ec9c327ca02
anhtan2223 <anhtan3332@gmail.com>
2024-03-30 16:13:31 +0700

    Script Database

--- Code/Frontend/src/views/HomeView.vue
+++ Code/Frontend/src/views/HomeView.vue
@@ -1,16 +1,9 @@
-<!-- <template>
-    <Sidebar/>
-</template> -->
 
 <script setup>
     import Sidebar from "../components/Sidebar.vue"
 </script>
 
-<template>
-    <div class="sidebar">
-        <Sidebar/>
-    </div>
-    
+<template>    
   <div class="container">
       <h1>
         HomeView
