6e52650de558166100b13c50b1b1a5a87d1a590c
PHP <phphong211@gmail.com>
2024-03-30 09:59:23 +0700

    sidebar

---
+++ Code/Frontend/src/views/components/Sidebar.vue
@@ -0,0 +1,21 @@
+<template>
+    <body class="wrapper">
+        <aside id="sidebar">
+            <div class="d-flex">
+                <button id="toggle-btn" type="button">
+                    <i class="lni lni-book"></i>
+                </button>
+                <div class="sidebar-logo">
+                    <a href="#">Library Management</a>
+                </div>
+            </div>
+        </aside>
+    </body>
+</template>
+
+<style>
+    
+</style>
+
+<script setup>
+</script>
