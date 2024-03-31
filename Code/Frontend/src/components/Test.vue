a25b5291304fcd3d48678474755eb80cdbd16340
anhtan2223 <anhtan3332@gmail.com>
2024-03-31 17:07:43 +0700

    Fix Test

--- Code/Frontend/src/components/Test.vue
+++ Code/Frontend/src/components/Test.vue
@@ -3,11 +3,7 @@
 </template>
 
 <script setup>
-import Sidebar from "../components/Sidebar.vue"
 import BookDesc from "./BookDescription.vue";
-import Navigation from "./Navigation.vue";
-import Acc from "../views/Admin/AdminAccount.vue"
-import Card from "../components/AccountCard.vue"
 </script>
 
 <style scoped>
