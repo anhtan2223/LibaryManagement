9ce6ec3182621a3a978ba1022b17b5c706d2aa8d
AurorantZ <zaloveliberty@gmail.com>
2024-04-01 23:54:25 +0700

    BorrowHistory/BorrowDetail

---
+++ Code/Frontend/src/views/Reader/BorrowDetail.Reader.vue
@@ -0,0 +1,66 @@
+<template>
+    <Nav></Nav>
+    <div class="container">
+        <h1><b style="color: #0d4dff">Chi tiết mượn trả</b></h1>
+        <div class="card mb-3" >
+            <div class="row g-0" >
+              <div class="col-md-4">
+                <img src="https://m.media-amazon.com/images/I/61tsrsmR7aS._SL1254_.jpg"  alt="..." style="width:100%"/>
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h1 class="card-title">Essential Computer Science</h1>
+                  <div>
+                    <div class="title">Người mượn</div>
+                    <div class="content">Nguyễn Phúc Tới</div>
+                  </div>
+                  <div>
+                    <div class="title">Mã thẻ thư viện</div>
+                    <div class="content">00000000000</div>
+                  </div>
+                  <div>
+                    <div class="title">Ngày mượn</div>
+                    <div class="content">00/00/0000</div>
+                  </div>
+                  <div>
+                    <div class="title">Nhân viên</div>
+                    <div class="content">Nguyễn Phúc Lui</div>
+                  </div>
+                  <div>
+                    <div class="title">Ngày hẹn trả</div>
+                    <div class="content">00/00/0000</div>
+                  </div>    
+                  <div>
+                    <div class="title">Trạng thái</div>
+                    <div class="content state">Đang mượn</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    margin-top: 80px;
+} 
+.title {
+    font-weight: bold;
+    font-size: 20px;
+    color: gray;
+}
+.content {
+    font-weight: bold;
+    font-size: 24px;
+    color: Black;
+    margin-bottom: 8px;
+}
+.state {
+    color: green;
+}
+</style>
+
+<script setup>
+    import Nav from '../../components/Navigation.Reader.vue'
+</script>
---
+++ Code/Frontend/src/views/Reader/BorrowHistory.Reader.vue
@@ -0,0 +1,73 @@
+<template>
+  <Nav></Nav>
+  <div class="container">
+      <h1><b style="color: #0d4dff">Lịch sử mượn trả</b></h1>
+      <div class="input-group rounded" style="margin-top: 8px">
+          <input type="search" class="form-control rounded" placeholder="Tìm kiếm sách" aria-label="Tìm kiếm sách" aria-describedby="search-addon" />
+          <span class="input-group-text border-0" id="search-addon">
+            <i class="fas fa-search"></i>
+          </span>
+      </div>
+      <table class="table table-borderless">
+          <thead>
+            <tr>
+              <th scope="col"></th>
+              <th scope="col">Tiêu đề</th>
+              <th scope="col">Số lượng</th>
+              <th scope="col">Thời gian mượn</th>
+              <th scope="col">Ngày hẹn trả</th>
+              <th scope="col">Trạng thái</th>
+              <th scope="col"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>The Pragmatic Programmer</td>
+              <td>1</td>
+              <td>31/3/2024</td>
+              <td>1/4/2024</td>
+              <td class="borrowing-state">Đang mượn</td>
+              <td>Xem chi tiết</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>The Pragmatic Programmer</td>
+              <td>1</td>
+              <td>31/2/2024</td>
+              <td>1/3/2024</td>
+              <td class="late-return-state">Quá hạn</td>
+              <td>Xem chi tiết</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>The Pragmatic Programmer</td>
+              <td>1</td>
+              <td>31/2/2024</td>
+              <td>1/3/2024</td>
+              <td class="returned-state">Đã trả</td>
+              <td>Xem chi tiết</td>
+            </tr>
+          </tbody>
+        </table>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  margin-top: 80px;
+}
+.borrowing-state {
+  color: #339900;
+}
+.late-return-state {
+  color: #cc3300;
+}
+.returned-state {
+  color: #0d4dff
+}
+</style>
+
+<script setup>
+  import Nav from '../../components/Navigation.Reader.vue'
+</script>
